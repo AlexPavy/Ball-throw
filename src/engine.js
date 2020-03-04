2f49e7c5a60a65c35c1f5a68420d64fce71b278f
Alex Pavy <alex.g.pavy@gmail.com>
2020-03-04 15:04:32 +0000

    added unit tests

--- src/engine.js
+++ src/engine.js
@@ -1,45 +1,66 @@
-/** Static parameters */
-const REFRESH_INTERVAL = 3; // milliseconds
-const BALL_SIZE = 10; // px
-const GROUND_SLOW_FACTOR = 1.1; // no unit
-const G = 0.001; // px / milliseconds^2
-const ZERO_SPEED_THRESHOLD = 0.0005; // px / milliseconds
-const INITIAL_SPEED_FACTOR = 1; // px / milliseconds
-
-function randomSpeed() {
-  return (Math.random() - 0.5) * 2 * INITIAL_SPEED_FACTOR;
-}
+/** Optional parameters and their default values */
+const OPTIONAL_PARAMS = {
+  REFRESH_INTERVAL: 3, // milliseconds
+  BALL_SIZE: 10, // px
+  GROUND_SLOW_FACTOR: 1.1, // no unit
+  G: 0.001, // px / milliseconds^2
+  ZERO_SPEED_THRESHOLD: 0.0005, // px / milliseconds
+  INITIAL_SPEED_FACTOR: 1, // px / milliseconds
+  LOG_PERIOD: 1000, // milliseconds
+};
 
 class BallEngine {
 
-  constructor(frame, PANEL_WIDTH, PANEL_HEIGHT) {
+  constructor(frame, params) {
     this.frame = frame;
+    // required params
+    if (params.PANEL_HEIGHT === undefined || params.PANEL_WIDTH === undefined) {
+      throw new Error("PANEL_HEIGHT and PANEL_WIDTH are required")
+    }
     this.params = {
-      PANEL_HEIGHT: PANEL_HEIGHT, // px
-      PANEL_WIDTH: PANEL_WIDTH, // px
-      ZERO_HEIGHT_THRESHOLD: PANEL_HEIGHT / 1000.0, // no unit
-      MAX_Y: PANEL_HEIGHT - BALL_SIZE, // px
-      MIN_Y: BALL_SIZE, // px
-      MAX_X: PANEL_WIDTH - BALL_SIZE, // px
-      MIN_X: BALL_SIZE, // px
+      PANEL_HEIGHT: params.PANEL_HEIGHT, // px
+      PANEL_WIDTH: params.PANEL_WIDTH, // px
     };
+
+    // optional params
+    for (let k in OPTIONAL_PARAMS) {
+      if (params[k] !== undefined) {
+        this.params[k] = params[k];
+      } else {
+        this.params[k] = OPTIONAL_PARAMS[k]
+      }
+    }
+
+    // calculated params
+    this.params.ZERO_HEIGHT_THRESHOLD = this.params.PANEL_HEIGHT / 1000.0; // no unit
+    this.params.MAX_Y = this.params.PANEL_HEIGHT - this.params.BALL_SIZE; // px
+    this.params.MIN_Y = this.params.BALL_SIZE; // px
+    this.params.MAX_X = this.params.PANEL_WIDTH - this.params.BALL_SIZE; // px
+    this.params.MIN_X = this.params.BALL_SIZE; // px
+
+    console.log("BallEngine - Parameters", this.params)
   }
 
   createBall(ball, x, y, v_x, v_y) {
     if (v_x === undefined) {
-      v_x = randomSpeed();
+      v_x = this.randomSpeed();
     }
-    if (v_y  === undefined) {
-      v_y = randomSpeed();
+    if (v_y === undefined) {
+      v_y = this.randomSpeed();
     }
-    ball.style.width = BALL_SIZE + "px";
-    ball.style.height = BALL_SIZE + "px";
+    ball.style.width = this.params.BALL_SIZE + "px";
+    ball.style.height = this.params.BALL_SIZE + "px";
     ball.style.position = "absolute";
     ball.style["background-color"] = "#00801b";
     ball.style["border-radius"] = "50%";
     this.frame.appendChild(ball);
     const singleBall = new SingleBall(this.params, ball, x, y);
     singleBall.start(v_x, v_y);
+    return singleBall;
+  }
+
+  randomSpeed() {
+    return (Math.random() - 0.5) * 2 * this.params.INITIAL_SPEED_FACTOR;
   }
 }
 
@@ -54,71 +75,94 @@ class SingleBall {
     this.y = y; // px
     this.v_x = 0; // px / milliseconds
     this.v_y = 0; // px / milliseconds
-    this.t1 = null; // milliseconds
+
+    this.dt = null; // t - t1, milliseconds
+    this.t1 = null; // previous time, milliseconds
+    this.lastLogTime = null; // last time where values were logged
+    this.nbBouncesX = 0; // number times it bounced on X axis
+    this.nbBouncesY = 0; // number times it bounced on Y axis
   }
 
-  start(v_x, v_y) {
+  async start(v_x, v_y) {
     this.v_x += v_x;
     this.v_y += v_y;
-    this.t1 = new Date().getTime(); // previous
+    this.setTime();
     this.display();
     this.ball.style.display = "block";
-    this.move();
+
+    while (!this.isNearZeroSpeed()) {
+      this.setTime();
+      this.logPeriodically();
+      this.move();
+      this.display();
+      await timeout(this.params.REFRESH_INTERVAL);
+    }
+    this.stop();
+    return this;
   }
 
   move() {
-    const now = new Date().getTime();
-    const t = now - this.t1;
-    this.t1 = now;
-    console.log(
-      `t:${t} ; x:${this.x} ; y:${this.y} ; v_x:${this.v_x} ; v_y:${this.v_y}`
-    );
+    this.v_y += this.params.G * this.dt;
+    this.y += this.v_y * this.dt;
+    this.x += this.v_x * this.dt;
 
-    if (this.stopIfNearZeroSpeed()) return;
-
-    this.v_y += G * t;
-    this.y += this.v_y * t;
-    this.x += this.v_x * t;
-    this.bounceIfOnGround();
-
-    this.display();
-    setTimeout(this.move.bind(this), REFRESH_INTERVAL);
-  }
-
-  stopIfNearZeroSpeed() {
-    if (
-      (this.params.MAX_Y - this.y) < this.params.ZERO_HEIGHT_THRESHOLD
-      && Math.abs(this.v_y) < ZERO_SPEED_THRESHOLD
-      && Math.abs(this.v_x) < ZERO_SPEED_THRESHOLD
-    ) {
-      this.v_x = 0;
-      this.v_y = 0;
-      this.display();
-      return true;
-    }
-  }
-
-  bounceIfOnGround() {
+    // bounce if on ground
     if (this.y > this.params.MAX_Y) {
       this.y = 2 * this.params.MAX_Y - this.y;
-      this.v_y = -(this.v_y / GROUND_SLOW_FACTOR);
-      this.v_x = this.v_x / GROUND_SLOW_FACTOR;
+      this.v_y = -(this.v_y / this.params.GROUND_SLOW_FACTOR);
+      this.v_x = this.v_x / this.params.GROUND_SLOW_FACTOR;
+      this.nbBouncesY++;
     }
     if (this.y < this.params.MIN_Y) {
       this.y = 2 * this.params.MIN_Y - this.y;
-      this.v_y = -(this.v_y / GROUND_SLOW_FACTOR);
-      this.v_x = this.v_x / GROUND_SLOW_FACTOR;
+      this.v_y = -(this.v_y / this.params.GROUND_SLOW_FACTOR);
+      this.v_x = this.v_x / this.params.GROUND_SLOW_FACTOR;
+      this.nbBouncesY++;
     }
     if (this.x > this.params.MAX_X) {
       this.x = 2 * this.params.MAX_X - this.x;
-      this.v_y = this.v_y / GROUND_SLOW_FACTOR;
-      this.v_x = -(this.v_x / GROUND_SLOW_FACTOR);
+      this.v_y = this.v_y / this.params.GROUND_SLOW_FACTOR;
+      this.v_x = -(this.v_x / this.params.GROUND_SLOW_FACTOR);
+      this.nbBouncesX++;
     }
     if (this.x < this.params.MIN_X) {
       this.x = 2 * this.params.MIN_X - this.x;
-      this.v_y = this.v_y / GROUND_SLOW_FACTOR;
-      this.v_x = -(this.v_x / GROUND_SLOW_FACTOR);
+      this.v_y = this.v_y / this.params.GROUND_SLOW_FACTOR;
+      this.v_x = -(this.v_x / this.params.GROUND_SLOW_FACTOR);
+      this.nbBouncesX++;
     }
+  }
+
+  setTime() {
+    const now = new Date();
+    if (this.t1) this.dt = now.getTime() - this.t1;
+    this.t1 = now.getTime();
+  }
+
+  logPeriodically() {
+    if ((this.t1 - this.lastLogTime) > this.params.LOG_PERIOD) {
+      console.log(
+        `dt:${this.dt} ; x:${this.x} ; y:${this.y} ; v_x:${this.v_x} ; v_y:${this.v_y}`
+      );
+      this.lastLogTime = this.t1;
+    }
+  }
+
+  isNearZeroSpeed() {
+    if (
+      (this.params.MAX_Y - this.y) < this.params.ZERO_HEIGHT_THRESHOLD
+      && Math.abs(this.v_y) < this.params.ZERO_SPEED_THRESHOLD
+      && Math.abs(this.v_x) < this.params.ZERO_SPEED_THRESHOLD
+    ) {
+      return true
+    }
+  }
+
+  stop() {
+    this.y = this.params.MAX_Y;
+    this.v_x = 0;
+    this.v_y = 0;
+    this.display();
   }
 
   display() {
@@ -127,10 +171,14 @@ class SingleBall {
   }
 }
 
+function timeout(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// Should use a web framework instead
 if (window) {
   window.BallEngine = BallEngine;
 }
-
 if (module) {
   module.exports = {
     BallEngine,
--- src/engine.test.js
+++ src/engine.test.js
@@ -1,16 +1,143 @@
 const engine = require('./engine');
 
-test('SingleBall constructor', () => {
-  const ballEngine = new engine.SingleBall({}, {style: {}}, 1, 1);
-  ballEngine.move();
-  expect(ballEngine.x).toBe(1);
+const TEST_PANEL_HEIGHT = 30;
+const TEST_PANEL_WIDTH = 30;
+const TEST_BALL_SIZE = 2;
+
+function getTestParams() {
+  const params = {
+    PANEL_HEIGHT: TEST_PANEL_HEIGHT,
+    PANEL_WIDTH: TEST_PANEL_WIDTH,
+    BALL_SIZE: TEST_BALL_SIZE,
+    ZERO_HEIGHT_THRESHOLD: TEST_PANEL_HEIGHT / 1000.0,
+    MAX_Y: TEST_PANEL_HEIGHT - TEST_BALL_SIZE,
+    MIN_Y: TEST_BALL_SIZE,
+    MAX_X: TEST_PANEL_WIDTH - TEST_BALL_SIZE,
+    MIN_X: TEST_BALL_SIZE,
+  };
+  const ballEngine = new engine.BallEngine(null, params);
+  return ballEngine.params;
+}
+
+test('SingleBall one tick on v_x', () => {
+  const ball = document.createElement("span");
+  const singleBall = new engine.SingleBall(getTestParams(), ball, 15, 15);
+  singleBall.start(1, 0);
+  expectAlmostEqual(singleBall.x, 17);
+  expectAlmostEqual(singleBall.y, 15);
+  singleBall.stop();
 });
 
-test('SingleBall can move', (done) => {
-  const ballEngine = new engine.SingleBall({}, {style: {}}, 1, 1);
-  ballEngine.move();
-  setTimeout(function () {
-    expect(ballEngine.x).toBe(0);
-    done();
-  }, 1000);
+test('SingleBall one tick on v_y', () => {
+  const ball = document.createElement("span");
+  const singleBall = new engine.SingleBall(getTestParams(), ball, 15, 15);
+  singleBall.start(0, -1);
+  expectAlmostEqual(singleBall.x, 15);
+  expectAlmostEqual(singleBall.y, 14);
+  singleBall.stop();
 });
+
+test('SingleBall wait bounce down', async () => {
+  const ball = document.createElement("span");
+  const singleBall = new engine.SingleBall(getTestParams(), ball, 15, 5);
+  singleBall.start(0, 0.02);
+  await timeout(200);
+  expect(singleBall.nbBouncesX).toBe(0);
+  expect(singleBall.nbBouncesY).toBe(1);
+  expectAlmostEqual(singleBall.x, 15);
+  expectAlmostEqual(singleBall.y, 26.3);
+  singleBall.stop();
+});
+
+test('SingleBall wait bounce right', async () => {
+  const ball = document.createElement("span");
+  const singleBall = new engine.SingleBall(getTestParams(), ball, 15, 5);
+  singleBall.start(0.07, -0.02);
+  await timeout(200);
+  expectAlmostEqual(singleBall.x, 26.9);
+  expectAlmostEqual(singleBall.y, 21.6);
+  expect(singleBall.nbBouncesX).toBe(1);
+  expect(singleBall.nbBouncesY).toBe(0);
+  singleBall.stop();
+});
+
+test('BallEngine bounce until stops', async () => {
+  const ball = document.createElement("span");
+  const params = getTestParams();
+  params.GROUND_SLOW_FACTOR = 2; // have test fast enough
+  const singleBall = new engine.SingleBall(params, ball, 15, 5);
+  await singleBall.start(0.1, -0.02);
+  expectAlmostEqual(singleBall.x, 14);
+  expectAlmostEqual(singleBall.y, 28);
+  expect(singleBall.nbBouncesX).toBe(1);
+  // Total number of Y-bounces is actually random
+  expect(singleBall.nbBouncesY).toBeGreaterThan(4);
+  singleBall.stop();
+});
+
+test('BallEngine create ball random speed eventually stops', async () => {
+  const frame = document.createElement("div");
+  const params = getTestParams();
+  params.GROUND_SLOW_FACTOR = 4; // have test fast enough
+  const ballEngine = new engine.BallEngine(frame, params);
+  const ball = document.createElement("span");
+  const singleBall = ballEngine.createBall(ball, 5, 27);
+  await timeout(4800);
+  expect(singleBall.isNearZeroSpeed()).toBeTruthy();
+  expect(singleBall.nbBouncesY).toBeGreaterThan(2)
+});
+
+test('BallEngine constructor required parameters', async () => {
+  try {
+    new engine.BallEngine(null, {PANEL_WIDTH: 400});
+    fail("Should require parameter");
+  } catch (e) {
+    expect(e.message).toBe("PANEL_HEIGHT and PANEL_WIDTH are required")
+  }
+});
+
+test('BallEngine constructor calculated parameters', async () => {
+  const ballEngine = new engine.BallEngine(null, {
+    PANEL_WIDTH: 400,
+    PANEL_HEIGHT: 500,
+    BALL_SIZE: 3,
+    MAX_Y: 77, // ignored
+    MIN_X: 77, // ignored
+  });
+  const params = ballEngine.params;
+  expect(params.PANEL_WIDTH).toBe(400);
+  expect(params.PANEL_HEIGHT).toBe(500);
+  expect(params.ZERO_HEIGHT_THRESHOLD).toBe(0.5);
+  expect(params.MAX_Y).toBe(497);
+  expect(params.MIN_Y).toBe(3);
+  expect(params.MAX_X).toBe(397);
+  expect(params.MIN_X).toBe(3);
+});
+
+test('BallEngine constructor optional parameters', async () => {
+  const ballEngine = new engine.BallEngine(null, {
+    PANEL_WIDTH: 400,
+    PANEL_HEIGHT: 500,
+    G: 0.004, // override
+    LOG_PERIOD: 5000, // override
+    INVALID_PARAM: 77 // ignored
+  });
+  const params = ballEngine.params;
+  expect(params.G).toBe(0.004);
+  expect(params.LOG_PERIOD).toBe(5000);
+  expect(params.BALL_SIZE).toBe(10);
+  expect(params.INITIAL_SPEED_FACTOR).toBe(1);
+  expect(params.REFRESH_INTERVAL).toBe(3);
+  expect(params.GROUND_SLOW_FACTOR).toBe(1.1);
+  expect(params.ZERO_SPEED_THRESHOLD).toBe(0.0005);
+  expect(params.INVALID_PARAM).toBe(undefined);
+});
+
+function expectAlmostEqual(expected, actual) {
+  const diff = Math.abs(expected - actual);
+  expect(diff).toBeLessThan(2);
+}
+
+function timeout(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
